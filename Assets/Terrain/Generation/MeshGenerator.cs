9fb0d42a476280527775351f4c37149bf84d0d33
Timon Dubreuil <timondubreuil@outlook.com>
2024-09-10 13:20:32 +0200

    manual normals calculation

--- Assets/Terrain/Generation/MeshGenerator.cs
+++ Assets/Terrain/Generation/MeshGenerator.cs
@@ -39,20 +39,20 @@ public class TerrainMeshData
     public int[] triangles;
     public Vector2[] uvs;
 
-    public  int triangleIndex = 0;
+    public int triangleIndex = 0;
 
-    public TerrainMeshData(int meshWidth,int meshLength)
+    public TerrainMeshData(int meshWidth, int meshLength)
     {
-        vertices = new Vector3[meshLength*meshWidth];
-        uvs = new Vector2[meshLength*meshWidth];
-        triangles = new int[(meshLength-1)*(meshWidth-1)*6];
+        vertices = new Vector3[meshLength * meshWidth];
+        uvs = new Vector2[meshLength * meshWidth];
+        triangles = new int[(meshLength - 1) * (meshWidth - 1) * 6];
     }
 
-    public void AddTriangle(int a,int b, int c)
+    public void AddTriangle(int a, int b, int c)
     {
         triangles[triangleIndex] = a;
-        triangles[triangleIndex+1] = b;
-        triangles[triangleIndex+2] = c;
+        triangles[triangleIndex + 1] = b;
+        triangles[triangleIndex + 2] = c;
         triangleIndex += 3;
     }
 
@@ -62,7 +62,44 @@ public class TerrainMeshData
         mesh.vertices = vertices;
         mesh.triangles = triangles;
         mesh.uv = uvs;
-        mesh.RecalculateNormals();
+        mesh.normals = CalculateNormals();
         return mesh;
+    }
+
+    private Vector3[] CalculateNormals()
+    {
+        Vector3[] vertexNormals = new Vector3[vertices.Length];
+        int triangleCount = triangles.Length / 3;
+        for (int i = 0; i < triangleCount; i++)
+        {
+            int normalTriangleIndex = i * 3;
+            int vertexIndexA = triangles[normalTriangleIndex];
+            int vertexIndexB = triangles[normalTriangleIndex + 1];
+            int vertexIndexC = triangles[normalTriangleIndex + 2];
+
+            Vector3 triangleNormal = SurfaceNormalFromIndices(vertexIndexA, vertexIndexB, vertexIndexC);
+            vertexNormals[vertexIndexA] += triangleNormal;
+            vertexNormals[vertexIndexB] += triangleNormal;
+            vertexNormals[vertexIndexC] += triangleNormal;
+        }
+
+        for (int i = 0; i < vertexNormals.Length; i++)
+        {
+            vertexNormals[i].Normalize();
+        }
+
+        return vertexNormals;
+    }
+
+    private Vector3 SurfaceNormalFromIndices(int indexA, int indexB, int indexC)
+    {
+        Vector3 pointA = vertices[indexA];
+        Vector3 pointB = vertices[indexB];
+        Vector3 pointC = vertices[indexC];
+
+        Vector3 sideAB = pointB - pointA;
+        Vector3 sideAC = pointC - pointA;
+
+        return Vector3.Cross(sideAB, sideAC).normalized;
     }
 }
